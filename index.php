cc653ed1ab7cf2db0e1437e4928b5717d068e0a6
Simon <simonhammes.27@gmail.com>
2019-08-05 21:48:35 +0200

    initial commit

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
 <?php
 
-/*
- Plugin Name: My Plugin
-*/
+// Plugin Name: wp-gb-plugin-template
 
-function shortcode_handler($attributes) {
+function handle_shortcode($attributes) {
 
-	// TODO https://richjenks.com/unnamed-wordpress-shortcode-attributes/
-	$a = shortcode_atts(['id' => '', 'flag' => ''], $attributes);
+	// $a = shortcode_atts(['id' => '', 'flag' => ''], $attributes);
 
 	return 'here: block is rendered';
 }
 
-add_shortcode('person', 'shortcode_handler');
+add_shortcode('person', 'handle_shortcode');
 
 function register_block() {
 
@@ -22,7 +19,7 @@ function register_block() {
 		['wp-blocks', 'wp-editor', 'wp-element', 'wp-components', 'wp-compose', 'wp-date']
 	);
 
-	register_block_type('namespace/person', [
+	register_block_type('dev/person', [
 		'attributes' => [
 			'id' => [
 				'type' => 'string',
@@ -34,7 +31,7 @@ function register_block() {
 			]
 		],
 		'editor_script' => 'block_script',
-		'render_callback' => 'shortcode_handler'
+		'render_callback' => 'handle_shortcode'
 	]);
 
 }
