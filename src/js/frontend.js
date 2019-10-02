c6235cea00be4b9773c77ac6c121cc0bc264529b
Simon <simonhammes.27@gmail.com>
2019-10-02 17:59:57 +0200

    fixed indentation

--- src/js/frontend.js
+++ src/js/frontend.js
@@ -2,9 +2,9 @@ import $ from 'jquery'
 
 $(function() {
 
-   $('.accordion-item-title').on('click', function() {
-      $(this).siblings('.accordion-item-content').slideToggle();
-   });
+    $('.accordion-item-title').on('click', function() {
+        $(this).siblings('.accordion-item-content').slideToggle();
+    });
 
 
 });
