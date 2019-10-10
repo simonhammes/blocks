f8d095c9d643a6591caedd674d1610a6e3843c53
Simon <simonhammes.27@gmail.com>
2019-10-10 22:52:26 +0200

    webpack.config.js follows the official wp eslint rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
+const glob = require( 'glob' );
+
 module.exports = {
-    devtool: 'source-map',
-    mode: 'development',
-    entry: {
-        backend: './src/backend.js',
-        frontend: './src/frontend.js',
-    },
-    output: {
-        path: __dirname + '/build/',
-        filename: '[name].build.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }
-        ]
-    },
-    externals: {
-        react: 'React',
-        'react-dom': 'ReactDOM',
-        jquery: 'jQuery'
-    },
-    stats: 'minimal'
+	devtool: 'source-map',
+	mode: 'development',
+	entry: {
+		backend: glob.sync( './src/js/backend/**/*.jsx' ),
+		frontend: glob.sync( './src/js/frontend/**/*.jsx' ),
+	},
+	output: {
+		path: __dirname + '/build/',
+		filename: '[name].build.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+		],
+	},
+	externals: {
+		react: 'React',
+		'react-dom': 'ReactDOM',
+		jquery: 'jQuery',
+	},
+	stats: 'minimal',
 };
